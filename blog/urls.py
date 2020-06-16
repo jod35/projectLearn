a29c38d0ba420292d056b52781436ee9e229a0fc
jod35 <jodestrevin@gmail.com>
2020-06-16 00:12:02 -0400

    created the first view and template

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/loggedout.html'), name='loggedout'),
+
+]
