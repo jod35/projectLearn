6bbea02ac82e6b970d7a0930c6809ae6a85a3e52
jod35 <jodestrevin@gmail.com>
2020-06-16 01:51:16 -0400

    added a login,signup and homepage view

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('signup/', views.signup, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/loggedout.html'), name='loggedout'),
 
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def signup(request):
+    return render(request, 'blog/signup.html')
