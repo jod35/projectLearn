5b220cab4a0ad1d55eed8cd11d00ac05dc0d9fab
jod35 <jodestrevin@gmail.com>
2020-06-18 08:02:04 -0400

    worked on comment form fucntionality

--- blog/static/blog/index.js
+++ blog/static/blog/index.js
@@ -1,8 +1,20 @@
 const comment_form= document.querySelector('#comment-form');
 const comment_button=document.querySelector('#comment-btn');
+const cancel_button=document.querySelector('.cancel-btn');
 
 comment_button.addEventListener('click',displayForm);
+cancel_button.addEventListener('click',removeForm);
+
+
+
+
 
 function displayForm(){
     comment_form.style.display="block";
+}
+
+function removeForm(){
+    if (comment_form.style.display=="block"){
+        comment_form.style.display="none";
+    }
 }
